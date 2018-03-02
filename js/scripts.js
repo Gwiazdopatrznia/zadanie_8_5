510412edd8ff07b7a7e292defc67650188b2dcb8
Anna Gajewska <gajewska.anna@o2.pl>
2018-03-02 20:43:01 +0100

    Add triangleArea script

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Enter a'),
+	h = prompt('Enter h'),
+	triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
